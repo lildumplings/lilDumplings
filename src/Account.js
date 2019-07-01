35a73ec6105e099670a69cc7f8257e678e104187
jinc132 <jinc132@u.washington.edu>
2019-06-30 22:30:29 -0700

    Add routing

---
+++ src/Account.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Account extends Component {
+    constructor(props) {
+      super(props);
+    }
+
+  render() {
+      return (
+        <div>
+           Coming Soon!
+        </div>
+      );
+  }
+}
+
+export default Account;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './css/App.css';
+import Account from './Account.js';
 
 function App() {
+  let renderHome = () => {
+    return (
+      <div>
+        This is the home!
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <h1>Coming Soon!</h1>
+      <a href='/Account'>
+        <button>Here</button>
+      </a>
+      <Switch>
+        <Route exact path={"/"} render={renderHome} />
+        <Route path={"/Account"} component={Account} />
+        <Redirect to={"/"} />
+      </Switch>
     </div>
   );
 }
