74cefba94961e48abade2387467b74487d0d3526
jinc132 <jinc132@u.washington.edu>
2019-07-08 18:34:05 -0700

    Add new files

---
+++ src/createAccount.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class createAccount extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: ""
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        Coming Soon!
+      </div>
+    );
+  }
+}
+
+export default createAccount;
